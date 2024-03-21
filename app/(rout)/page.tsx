231c5cd547e0c57feb20b9c01a9882f0796de17f
diego <diego@MacBook-Pro-de-diego.local>
2024-03-21 11:15:04 -0300

    ADD: sidebarleft, topbar, and links for the sidebarleft and button logout

--- app/(rout)/page.tsx
+++ app/(rout)/page.tsx
@@ -1,8 +1,9 @@
- 
+ import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return (
-    <div className="h-screen">
-      <h1 className="head-text text-left">Home</h1>
-    </div>
+    <>
+      <h1 className="head-text text-left" >Home</h1>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+    </>
   )
 }
